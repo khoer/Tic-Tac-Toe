3f7cf63aa0bc036c8d9f8d31b19209b95ff12e1a
Khoerudin <khoe.tw@gmail.com>
2015-07-09 17:18:34 +0700

    Create TicTacToeTest.java

---
+++ src/creation/sahif/TicTacToeTest.java
@@ -0,0 +1,15 @@
+package creation.sahif;
+
+/*
+ *This program to test TicTacToe
+ *@author: Khoerudin
+ */
+
+public class TicTacToeTest {
+
+	public static void main(String[] args) {
+		TicTacToe ttt = new TicTacToe();
+		ttt.printBoard();
+		ttt.play();
+	}
+}
